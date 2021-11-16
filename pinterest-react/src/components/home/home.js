9cf40662bd2194aec303763e7323e23c92497d0b
paovats <paovats@ciencias.unam.mx>
2021-11-16 13:15:48 -0600

    start home ui

--- pinterest-react/src/components/home/home.js
+++ pinterest-react/src/components/home/home.js
@@ -2,9 +2,11 @@ import {signOut} from "firebase/auth"
 import { auth } from "../../firebase-config";
 import { useNavigate } from "react-router-dom"
 import imgur from "../../api/imgur";
+import { useEffect, useState } from "react";
 
 
 const Home =()=>{
+    const [response , setResponse]= useState("")
     const navigate = useNavigate();
     const logout =async()=>{
         signOut(auth).then(() => {
@@ -13,12 +15,20 @@ const Home =()=>{
             alert("error")
           });
     }
-    const search=()=>{
+    useEffect(()=>{
+        if(response!==""){
+            console.log(response)
+        }
+    },[response])
+    const search=async()=>{
         return imgur.get("https://api.imgur.com/3/gallery/t/gaming/time/week/2")
+            .then((result)=> setResponse(result.data.data.items))
+            .catch((error)=>{alert(error)})
     }
+    
     return(
         <>
-        <h1>HOLA HOME</h1>
+        <img src='../../Pinterest-logo.png'></img>
         <button onClick={search}>probando logout</button>
         </>
     )
