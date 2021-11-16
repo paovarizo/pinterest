30efb9e2e7340e6c8d532dd7d5a1676339af3c1c
paovats <paovats@ciencias.unam.mx>
2021-11-16 08:59:37 -0600

    conect imgur api to the frontend

--- pinterest-react/src/components/home/home.js
+++ pinterest-react/src/components/home/home.js
@@ -1,6 +1,9 @@
 import {signOut} from "firebase/auth"
 import { auth } from "../../firebase-config";
 import { useNavigate } from "react-router-dom"
+import imgur from "../../api/imgur";
+
+
 const Home =()=>{
     const navigate = useNavigate();
     const logout =async()=>{
@@ -10,10 +13,13 @@ const Home =()=>{
             alert("error")
           });
     }
+    const search=()=>{
+        return imgur.get("https://api.imgur.com/3/gallery/t/gaming/time/week/2")
+    }
     return(
         <>
         <h1>HOLA HOME</h1>
-        <button onClick={logout}>probando logout</button>
+        <button onClick={search}>probando logout</button>
         </>
     )
 }
